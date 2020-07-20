09c4c45ced975bce8e5fbfd9db6739e9f5085713
Frog Chen <satgo1546@yeah.net>
2020-07-20 12:09:58 +0800

    slzite: upload

--- _slzite.rb
+++ _slzite.rb
@@ -12,7 +12,7 @@ end
 def apply_template(template, contents, dest_filename)
   r = template.chomp + "\n"
   nil while r.gsub!(/^\s*#include\s+[<"](.+)[>"]\s*$/) { File.read("src/modules/#{$1}") }
-  r.gsub!(/(href|src)=\"\/(.+?)\"/i) { "%s=\"%s\"" % [$1, Pathname.new($2).relative_path_from(Pathname.new(File.dirname(dest_filename)))] }
+  r.gsub!(/(href|src)=\"\/(.*?)\"/i) { "%s=\"%s\"" % [$1, Pathname.new($2).relative_path_from(Pathname.new(File.dirname(dest_filename)))] }
   r.gsub!(/^\s*#pragma\s+CONTENTS\s*$/, contents)
 end
 
@@ -71,7 +71,7 @@ def upload
   end
 end
 
-def interface
+def check
   if not Dir.exist?(".git")
     Dir.chdir(__dir__)
     puts "工作目录不在一个git存储库顶端。已切换到_slzite.rb所在目录。"
@@ -87,31 +87,38 @@ def interface
   if not /\d+\.\d+\.\d+/ =~ `sass --version`
     raise UIErrorMessage.new("似乎没有安装SASS，或者我不能使用。请确认已经安装SASS命令行程序并全局可用。")
   end
-  option = ARGV[0]
-  if not option
+end
+
+def interface(first_run)
+  if first_run
     puts <<~EOF
-      \r欢迎！slzite是将使用Markdown、SASS、HTML模板技术制作的网站生成为浏览器可以直接查看的网页文件集的工具。注意，本工具与博客并无直接关系。
+      \r欢迎！slzite是将使用Markdown、SASS、HTML模板技术制作的网站生成为浏览器可以直接查看的网页文件集的工具。
       警告：网站内容在src目录中。该目录外的内容会随时被本工具覆盖！
-      slzite is a tool for generating websites made with Markdown, SASS, and HTML template technology into a set of webpage files that can be viewed directly by the browser. Note that this tool is not directly related to the blog.
-
-      请选择你的英雄：
-      [1] 预览
-      [2] 上传
-      [3] 只生成而不预览或上传
-      [4] 开始编写一篇博客文章
-      [9] slzite的原理
-      [0] 退出
-
-      Please choose your operation:
-      [1] Preview
-      [2] Upload
-      [3] Only generate without previewing or uploading
-      [4] Start writing a blog post
-      [9] How slzite works
-      [0] Exit
+      slzite is a tool for generating websites made with Markdown, SASS, and HTML template technology into a set of webpage files that can be viewed directly by the browser.
+      Warning: Website contents reside in src/. Files outside may be overwritten by this tool at any time.
     EOF
-    option = $stdin.getch
   end
+  puts <<~EOF
+    请选择你的英雄：
+    [1] 预览
+    [2] 上传
+    [3] 只生成而不预览或上传
+    [4] 开始编写一篇博客文章
+    [9] slzite的原理
+    [0] 退出
+
+    Please choose your operation:
+    [1] Preview
+    [2] Upload
+    [3] Only generate without previewing or uploading
+    [4] Start writing a blog post
+    [9] How slzite works
+    [0] Exit
+  EOF
+  $stdin.getch
+end
+
+def run(option)
   case option
   when "1"
     generate
@@ -147,11 +154,24 @@ def interface
     else
       system ENV["EDITOR"], filename
     end
+  when "9"
+    puts <<~EOF
+      　　假如你是李华，你要基于GitHub Pages服务制作静态个人网站，但是觉得Jekyll太难用，又不想直接写HTML和CSS。你在lihua.github.io存储库的src/index.md里写好自我介绍，在src/stylesheet.scss里编写好网站样式，在src/modules/navbar.html里制作好网站导航条，写出网页模板src/modules/default.html：
+        <title>李华的个人网站</title>
+        <link rel="stylesheet" src="/stylesheet.css">
+        <body>
+          #include "navbar.html"
+          <main>
+            #pragma CONTENTS
+      　　假如你的英语作文放在src/essay/english/001.md，如果你愿意，可为之使用模板src/modules/essay_english.html，而不是src/modules/default.html。slzblog会处理src目录，自动转换Markdown和SASS文件并套上模板。模板中常见的以/开头的链接也会被替换为相对路径，这样就能直接打开本地网页预览了。如果要直接发布，生成、提交、推送也能自动一气呵成。
+      　　按任意键继续……
+    EOF
+    $stdin.getch
   when "0"
     puts "即将退出。"
+    exit
   else
-    puts "未知的选项，按任意键退出。"
-    $stdin.getch
+    puts "未知的选项，请重试。"
   end
 rescue => exception
   puts "Something happened."
@@ -163,7 +183,5 @@ rescue => exception
   end
 end
 
-
-while 1
-  interface
-end
+run(ARGV[0] || interface(true))
+run(interface(false)) until ARGV[0]
