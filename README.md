8e24c044de513b450767e9ac76f4b125ae8de840
Frog Chen <satgo1546@yeah.net>
2022-11-25 20:14:39 +0800

    Add template.php

--- README.md
+++ README.md
@@ -3,18 +3,18 @@
 
 - 共通部分
   - 模板导航条、预处理Markdown（含链接）和Sass、子页面目录
-- [salenzo.github.io ![GitHub Pages workflow](https://github.com/Salenzo/salenzo.github.io/actions/workflows/deploy.yml/badge.svg)](https://salenzo.github.io/)
+- [salenzo.github.io ![GitHub Actions workflow](https://github.com/Salenzo/salenzo.github.io/actions/workflows/deploy.yml/badge.svg)](https://salenzo.github.io/)
   - 特色：写得一派混乱的样式表，宽为*π*个空格的制表符轻而易举地得罪所有人
   - Jekyll + MathJax 2
 - [salenzo.readthedocs.io ![Documentation Status](https://readthedocs.org/projects/salenzo/badge/?version=latest)](https://salenzo.readthedocs.io/)
-  - 特色：MathML的CSS实现，自定义主题避免Read the Docs注入导航浮动条
-  - MkDocs + MathJax 3 (SSR → MathML)
-- [salenzo.neocities.org（在建） ![Gem](https://img.shields.io/gem/v/neocities?label=gem%20install%20neocities&logo=ruby)](https://salenzo.neocities.org/)
+  - 特色：MathML的CSS实现；自定义主题避免Read the Docs注入导航浮动条
+  - MkDocs + MathJax 3 (SSR → MathML) + Prism
+- [salenzo.neocities.org（在建） ![GitHub Actions workflow](https://github.com/Salenzo/salenzo.github.io/actions/workflows/deploy.yml/badge.svg)](https://salenzo.neocities.org/)
   - 特色：超古老的静态网站生成器工具链
-  - GTML + Markdown.pl
+  - GTML + Markdown.pl + LaTeX2HTML
 - [salenzo.vercel.app（在建）](https://salenzo.vercel.app/)
   - 特色：
-  - Next.js + MathJax 2 (SSR → SVG)
+  - Next.js + KaTeX (SSR)
 - [salenzo.gitlab.io（在建） ![Pipeline Status](https://gitlab.com/salenzo/salenzo.gitlab.io/badges/main/pipeline.svg)](https://salenzo.gitlab.io/)
-  - 特色：基于GitHub Actions的GitLab CI/CD运行器，不绑定银行卡也能将白嫖贯彻到底
-  - PHP
+  - 特色：基于GitHub Actions的GitLab CI/CD运行器，不绑定银行卡也能将白嫖贯彻到底；PHP也想作为静态网站生成器
+  - PHP + Michelf\\Markdown + MathJax 2
---
+++ template.php
@@ -0,0 +1,132 @@
+<?php if (isset($contents)): ?>
+<!DOCTYPE html>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title><?= empty($title) ? htmlspecialchars($title) . " — " : "" ?>Ŝalenzo</title>
+<link rel="shortcut icon" href="/assets/favicon.ico">
+
+<nav>
+  <a href="/">Ŝalenzo</a>
+  <?php foreach (array(
+		"Home" => "/",
+	) as $name => $url): ?>
+  	<a href="<?= htmlspecialchars($url) ?>"><?= htmlspecialchars($name) ?></a>
+  <?php endforeach; ?>
+</nav>
+
+<main>
+  <?php if ($metadata): ?>
+		<dl class="meta">
+			<?php foreach ($metadata as $key => $value): ?>
+				<dt><?= htmlspecialchars($key) ?></dt>
+				<dd><?= htmlspecialchars($value) ?></dd>
+			<?php endforeach; ?>
+		</dl>
+		<hr>
+  <?php endif; ?>
+	<?= $contents ?>
+</main>
+
+<aside>
+  <h1>About us</h1>
+  <a href="https://github.com/akhialomgir">シュサイ Akhia</a>
+  <a href="https://github.com/satgo1546">シュサイ satgo1546</a>
+  <a href="https://github.com/Ezioxk">シュサイ Ezioxk</a>
+  <a href="https://mochanaigai363.lofter.com">アート Flora</a>
+  <a href="https://github.com/Salenzo" class="more">more »</a>
+  <img src="/assets/img/MMPD.svg" alt="Ŝalenzo Logo" style="opacity: .3;">
+</aside>
+
+<footer>
+  Copyright © Ŝalenzo<br>
+  Powered by Ŝanity & <a href="https://www.php.net/"><img src="/assets/img/php-power-black.png" alt="Powered by PHP"></a> <?= phpversion() ?>
+</footer>
+
+<?php return;
+endif;
+
+require "vendor/autoload.php";
+use Michelf\MarkdownExtra;
+
+// Clear the contents of the destination folder.
+if (!file_exists("_site")) mkdir("_site");
+foreach (
+	new RecursiveIteratorIterator(
+		new RecursiveDirectoryIterator("_site", RecursiveDirectoryIterator::SKIP_DOTS),
+		RecursiveIteratorIterator::CHILD_FIRST,
+	) as $file
+) {
+	if ($file->isDir()) {
+		rmdir($file->getPathname());
+	} else {
+		unlink($file->getPathname());
+	}
+}
+
+$markdown = new MarkdownExtra;
+$markdown->hard_wrap = true;
+$markdown->url_filter_func = function ($url) {
+	return strtolower($url);
+};
+$markdown->header_id_func = function ($text) {
+	return preg_replace('/[^a-z0-9]/', '-', strtolower($text));
+};
+ini_set("highlight.comment", "#008000");
+ini_set("highlight.default", "#000000");
+ini_set("highlight.html", "#808080");
+ini_set("highlight.keyword", "#0000BB; font-weight: bold");
+ini_set("highlight.string", "#DD0000");
+$markdown->code_block_content_func = function ($code, $language) {
+	if (!$language) return htmlspecialchars($code);
+	if (str_contains($code, "<?php ")) {
+		$code = highlight_string($code, true);
+	} else {
+		$code = highlight_string("<?php " . $code, true);
+		$code = str_replace("&lt;?php&nbsp;", "", $code);
+	}
+	$code = str_replace("<code>", "", $code);
+	$code = str_replace("</code>", "", $code);
+	return trim($code);
+};
+$markdown->hashtag_protection = true;
+
+foreach (
+	new RecursiveIteratorIterator(
+		new RecursiveDirectoryIterator("src", RecursiveDirectoryIterator::SKIP_DOTS),
+	) as $src
+) {
+	echo $src->getPathname() . "\n";
+	$contents = file_get_contents($src->getPathname());
+	$title = "";
+	$metadata = array();
+	if ($src->getExtension() === "md") {
+		// Parse the front matter and populate $metadata.
+		if (str_starts_with($contents, "---\n")) {
+			[$front_matter, $contents] = explode("---\n", substr($contents, 4), 2);
+			foreach (explode("\n", $front_matter) as $line) if (str_contains($line, ":")) {
+				[$name, $value] = explode(":", trim($line, ":"), 2);
+				$metadata[trim($name)] = trim($value);
+			}
+			unset($front_matter, $line, $name, $value);
+		}
+		// Find the title of the document.
+		if (array_key_exists("title", $metadata)) {
+			$title = $metadata["title"];
+		} else {
+			// Find the first <h1>, <h2>, or <h3> in the Markdown source.
+			preg_match('/^\s*(?:#{1,3}\s+(.*)(?:\s+\#{1,3})?|(.*)\r?\n[-=]+\s*)$/m', $contents, $title);
+			$contents = $markdown->transform($contents);
+			$title = array_key_exists(1, $title) ? $title[1] : "";
+		}
+		// Use this program as a HTML template and PHP as a powerful templating engine.
+		ob_start();
+		include __FILE__;
+		$contents = ob_get_contents();
+		ob_end_clean();
+	}
+	$dest = str_replace("src", "_site", $src->getPathname());
+	$dest_dir = str_replace("src", "_site", $src->getPath());
+	if (!file_exists($dest_dir)) mkdir($dest_dir, 0777, true);
+	file_put_contents($dest, $contents);
+}
